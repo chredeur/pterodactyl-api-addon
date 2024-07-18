08cbb2b68e63eedf60e6439371c6764d3946cabe
chredeur <chredeur@pastanetwork.com>
2024-07-19 00:15:59 +0200

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Chredeur\PterodactylApiAddon\Http\Controllers\ServerTransfertApplicationController;
-use Pterodactyl\Http\Middleware\Api\Client\Server\ResourceBelongsToServer;
-use Pterodactyl\Http\Middleware\Api\Client\Server\AuthenticateServerAccess;
 
 Route::prefix('/api/application')->middleware(['api', 'throttle:api.application'])->group(function () {
 
-    Route::group(['prefix' => '/servers'], function () {
+    Route::group(['prefix' => '/transfer'], function () {
         /** Transfer Server */
-        Route::post('/{server:id}/transfer', [ServerTransfertApplicationController::class, 'transfer'])->name('api.application.servers.transfer');
+        Route::post('/{server:id}', [ServerTransfertApplicationController::class, 'transfer'])->name('api.application.transfer');
     });
 
 });
